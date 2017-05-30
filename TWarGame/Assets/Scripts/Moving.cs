200dcc4b34b5440dec76bfc3b88b7c9e4888eec4
RoranDangerous <1996roma@ukr.net>
2017-05-30 16:10:10 -0700

    New Project Started. Connected with PUN

---
+++ TWarGame/Assets/Scripts/Moving.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Moving : MonoBehaviour {
+
+    private new Rigidbody rigidbody;
+    private float startAccelerationX, startAccelerationZ;
+
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        startAccelerationX = Input.acceleration.x;
+        startAccelerationZ = Input.acceleration.z;
+    }
+	
+	void Update () {
+        //Vector3 movement = new Vector3(Input.acceleration.x - startAccelerationX, 0, Input.acceleration.z - startAccelerationZ);
+        rigidbody.velocity = new Vector3((Input.acceleration.x - startAccelerationX) * 25, 0, (Input.acceleration.z - startAccelerationZ) * (-25));
+    }
+}
---
+++ TWarGame/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Photon.PunBehaviour
+{
+
+    public string roomName = "testRoom";
+    const string version = "v0.0.1";
+    public GameObject spawn;
+    public string playerPrefab = "Player";
+
+    void Start () {
+        if (!PhotonNetwork.connected)
+            PhotonNetwork.ConnectUsingSettings(version);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        print("###### lobby joined!!! #########");
+        base.OnJoinedLobby();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = false;
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        print("###### Instantiating #######");
+        PhotonNetwork.Instantiate(playerPrefab, spawn.transform.position, spawn.transform.rotation, 0);
+    }
+}
