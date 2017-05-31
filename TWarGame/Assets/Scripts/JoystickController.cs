26fad89574113ee266d1c09023338d621ff48013
RoranDangerous <1996roma@ukr.net>
2017-05-31 14:35:13 -0700

    working on rotating the body

--- TWarGame/Assets/Scripts/JoystickController.cs
+++ TWarGame/Assets/Scripts/JoystickController.cs
@@ -12,6 +12,8 @@ public class JoystickController : MonoBehaviour, IDragHandler, IPointerUpHandler
     public GameObject weapon;
     public GameObject body;
     private Vector3 inputVector;
+    private Vector3 startAnchoredPosition;
+    public GameObject player;
 
     void Start () {
         outerJ = GetComponent<Image>();
@@ -22,7 +24,7 @@ public class JoystickController : MonoBehaviour, IDragHandler, IPointerUpHandler
     {
         Vector2 pos;
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(outerJ.rectTransform
-                       , new Vector2(ped.position.x - 100, ped.position.y + 100)
+                       , new Vector2(ped.position.x - 250, ped.position.y + 250)
                        , ped.pressEventCamera
                        , out pos))
         {
@@ -36,9 +38,13 @@ public class JoystickController : MonoBehaviour, IDragHandler, IPointerUpHandler
             innerJ.rectTransform.anchoredPosition =
              new Vector3(inputVector.x * (outerJ.rectTransform.sizeDelta.x / 3)
               , inputVector.z * (outerJ.rectTransform.sizeDelta.y / 3));
-
             //Rotate the weapon
-            weapon.transform.LookAt(new Vector3(innerJ.rectTransform.anchoredPosition.x,0, innerJ.rectTransform.anchoredPosition.y));
+            /*Vector3 target = new Vector3();
+            target.z = innerJ.rectTransform.anchoredPosition.y;
+            weapon.transform.LookAt(target);*/
+            //print((innerJ.rectTransform.anchoredPosition.x - player.transform.position.x)+" "+ (innerJ.rectTransform.anchoredPosition.y - player.transform.position.z));
+            //weapon.transform.LookAt(new Vector3(innerJ.rectTransform.anchoredPosition.x - player.transform.position.x,0, innerJ.rectTransform.anchoredPosition.y - player.transform.position.z));
+            weapon.transform.LookAt(new Vector3(-innerJ.rectTransform.anchoredPosition3D.x+14, 0, -innerJ.rectTransform.anchoredPosition3D.y+14));
         }
     }
 
--- TWarGame/Assets/Scripts/Moving.cs
+++ TWarGame/Assets/Scripts/Moving.cs
@@ -14,11 +14,15 @@ public class Moving : Photon.MonoBehaviour {
         rigidbody = GetComponent<Rigidbody>();
         startAccelerationX = Input.acceleration.x;
         startAccelerationZ = Input.acceleration.y;
-
     }
-	
-	void Update () {
-        if(np.myCar)
-            rigidbody.velocity = new Vector3((Input.acceleration.x - startAccelerationX) * 25, 0, (Input.acceleration.y - startAccelerationZ) * (25));
+
+    void Update() {
+        if (np.myCar)
+        {
+            rigidbody.velocity = new Vector3((Input.acceleration.x - startAccelerationX) * 20, 0, (Input.acceleration.y - startAccelerationZ) * 20);
+            /*if((Input.acceleration.x - startAccelerationX) > 0.1 || (Input.acceleration.y - startAccelerationZ) > 0.1)
+                body.transform.LookAt(new Vector3(body.transform.position.x + (Input.acceleration.x - startAccelerationX), body.transform.position.y, body.transform.position.z + (Input.acceleration.y - startAccelerationZ)));*/
+            
+        }
     }
 }
