8aaa74c5fd75c3f29a105c294645ca8d9b651f76
RoranDangerous <1996roma@ukr.net>
2017-06-20 14:04:24 -0700

    Started 2D

---
+++ TWar2D/Assets/Scripts/Moving.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Moving : Photon.MonoBehaviour {
+    
+    private float startAccelerationX, startAccelerationZ;
+    public NetworkPlayer np;
+    private GameObject body;
+    private Vector3 lastPosition;
+    private GameObject sphereO;
+    private float threshold = 0.01f;
+    private float speed = 70;
+    private float maxSpeed = 13;
+    private GameObject cam;
+    private GameObject weapon;
+    private GameObject healthCanv;
+
+    void Start () {
+        body = transform.Find("body").gameObject;
+        cam = transform.Find("PlayerCamera").gameObject;
+        weapon = transform.Find("weapon").gameObject;
+        healthCanv = transform.Find("healthAndReloadCanvas").gameObject;
+        //sphereO = body.transform.Find("SphereTarget").gameObject;
+        //sphereO.SetActive(false);
+        lastPosition = body.transform.position;
+
+        ChangeStartAcceleration();
+    }
+
+    void Update() {
+        MoveCar();
+    }
+
+    private void ChangeStartAcceleration()
+    {
+        startAccelerationX = Input.acceleration.x;
+        startAccelerationZ = Input.acceleration.y;
+    }
+
+    private void MoveCar()
+    {
+        if (np.myCar)
+        {
+            ChangeVelocity();
+
+            RotateCar();
+        }
+    }
+
+    private void ChangeVelocity()
+    {
+        //if(Input.acceleration.x > limit || Input.acceleration.x < -limit || Input.acceleration.x < -limit || Input.acceleration.y > limit)
+        //body.GetComponent<Rigidbody>().velocity = new Vector3(getVelocityX(),0,getVelocityZ());
+        //body.GetComponent<Rigidbody>().AddForce(new Vector3(getVelocityX(), 0, getVelocityZ()));
+		body.transform.position = new Vector3(body.transform.position.x+0.01f,body.transform.position.y+0.01f, body.transform.position.z);
+		cam.transform.position = new Vector3(body.transform.position.x,body.transform.position.y,cam.transform.position.z);
+        healthCanv.transform.position = new Vector3(body.transform.position.x, body.transform.position.y, body.transform.position.z-1);
+        weapon.transform.position = new Vector3(body.transform.position.x, body.transform.position.y, body.transform.position.z);
+    }
+
+    private void RotateCar()
+    {
+		GameObject sphere = body.transform.parent.Find ("Sphere").gameObject;
+		//body.transform.LookAt(new Vector3(sphere.x,0,sphere.y));
+        //if (Math.Abs(body.transform.position.x - lastPosition.x) > threshold || Math.Abs(body.transform.position.y - lastPosition.y) > threshold)
+        {
+			Vector3 lookPosition = new Vector3((body.transform.position.x - lastPosition.x) * speed + body.transform.position.x, 0, (body.transform.position.y - lastPosition.y) * speed + body.transform.position.y);
+			sphere.transform.position = lookPosition;
+            body.transform.LookAt(lookPosition);
+        }
+        lastPosition = body.transform.position;
+    }
+
+    private float getVelocityX()
+    {
+        if ((Input.acceleration.x - startAccelerationX) * speed > maxSpeed)
+        {
+            return maxSpeed;
+        }
+        else if ((Input.acceleration.x - startAccelerationX) * speed < -maxSpeed)
+        {
+            return -maxSpeed;
+        }
+        else return (Input.acceleration.x - startAccelerationX) * speed;
+    }
+
+    private float getVelocityZ()
+    {
+        if ((Input.acceleration.y - startAccelerationZ) * speed > maxSpeed)
+        {
+            return maxSpeed;
+        }
+        else if ((Input.acceleration.y - startAccelerationZ) * speed < -maxSpeed)
+        {
+            return -maxSpeed;
+        }
+        else return (Input.acceleration.y - startAccelerationZ) * speed;
+    }
+}
---
+++ TWar2D/Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShootingScript : Photon.MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private GameObject weapon;
+    private float startAngle = 15;
+    private float endAngle = -5;
+    private float timeLeft = 0;
+    private float reloadTime = 3f;
+    private GameObject hitObject;
+    private bool buttonPressed = false;
+
+    void Start()
+    {
+        SetReloadTime(3f);
+        AssignObjects();
+    }
+
+    private void Update()
+    {
+        DecreaseTime();
+
+        if (buttonPressed)
+        {
+            Shoot();
+        }
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        AnimateButtonUP();
+        buttonPressed = false;
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+		print ("DOWN");
+        AnimateButtonDown();
+        buttonPressed = true;
+    }
+
+    private void AssignObjects()
+    {
+        weapon = transform.root.transform.Find("weapon").gameObject;
+        GetComponent<Image>().rectTransform.anchoredPosition = new Vector3(-(transform.parent.GetComponent<RectTransform>().rect.width * .4f / 3), transform.parent.GetComponent<RectTransform>().rect.height * .6f / 3, 0);
+    }
+
+    private void DecreaseTime()
+    {
+        timeLeft -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        if (Reloaded())
+        {
+            timeLeft = reloadTime;
+            for (float i = startAngle; i >= endAngle; i -= 1)
+            {
+                if (ShootRaycast(i))
+                    break;
+            }
+        }
+    }
+
+    private bool Reloaded()
+    {
+        return timeLeft <= 0;
+    }
+
+    private bool ShootRaycast(float angle)
+    {
+        Vector3 targetPos = weapon.transform.position + (Quaternion.Euler(angle, 0, 0) * (weapon.transform.forward * -1)).normalized * 500;
+        RaycastHit hit;
+        //Debug.DrawRay(weapon.transform.position, targetPos * 1000f, Color.white, 20, true);
+        if (Physics.Raycast(weapon.transform.position, targetPos, out hit))
+        {
+            hitObject = hit.collider.gameObject.transform.root.gameObject;
+            if ( !isThis(hitObject) )
+            {
+                if(hitObject.GetComponent<PhotonView>() != null)
+                {
+                    hitObject.GetComponent<PhotonView>().RPC("ApplyDamage", PhotonPlayer.Find(hitObject.GetComponent<PhotonView>().owner.ID));
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+
+    private bool isThis(GameObject target)
+    {
+        return target == this.transform.root.gameObject;
+    }
+
+    public void SetReloadTime(float time)
+    {
+        reloadTime = time;
+    }
+
+    public float GetReloadTime()
+    {
+        return reloadTime;
+    }
+
+    public float GetTimeLeft()
+    {
+        return timeLeft;
+    }
+
+    private void AnimateButtonDown()
+    {
+        GetComponent<Image>().rectTransform.localScale = new Vector3(.9f,.9f,0);
+    }
+
+    private void AnimateButtonUP()
+    {
+        GetComponent<Image>().rectTransform.localScale = new Vector3(1, 1, 0);
+    }
+}
